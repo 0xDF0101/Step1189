668f46b58b537341a516a5fa39ac52b495db4100
EugeneTA <0xdf0101@gmail.com>
2026-04-16 15:52:11 +0900

    config: 설정 파일 좀 만짐

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,9 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
         SpringApplication.run(Main.class, args);
-
-
     }
 }
